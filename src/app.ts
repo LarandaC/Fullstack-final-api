e39d1ec4fb954c992f5ec25279bf03026c0b05d1
Victoria <106978058+LarandaC@users.noreply.github.com>
2026-04-11 21:40:13 -0300

    feat: aumentar el límite de tamaño para el cuerpo de las solicitudes JSON y mejorar la declaración de parámetros en la ruta raíz

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,9 @@ connectDB();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.json({ message: "API funcionando" });
 });
 
